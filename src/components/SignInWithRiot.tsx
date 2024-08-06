0489483c8f646144cacb95fb0ade77464c9fc125
Eduardo Kegler <eduardo.kegler@coinbase.com>
2024-08-06 18:07:30 -0300

    feat: fix build

--- src/components/SignInWithRiot.tsx
+++ src/components/SignInWithRiot.tsx
@@ -13,9 +13,6 @@ export const SignInWithRiot = memo(function SignInWithRiot({
   const response_type = "code";
   const scope = "openid";
 
-  const callback =
-    "http://localhost:3000/oauth/callback?code=dXcxOkNWeUkyaUU1QW1SRXV2NlUzaUZuOVEuYi0yLXVkR2ZIM1AyY29RY2J3V2VOZw%3D%3D&iss=https%3A%2F%2Fauth.riotgames.com&session_state=ytovGnZ8obUdk9acfTCW8tc6z4UauCcWtoW0TPM0toc.d8LvvSWjbTUa_mVcrVtXVQ";
-
   const handleSignIn = useCallback(() => {
     const url = `https://auth.riotgames.com/authorize?redirect_uri=${redirect_uri}&client_id=${clientId}&response_type=${response_type}&scope=${scope}`;
     window.open(url, "_blank");
