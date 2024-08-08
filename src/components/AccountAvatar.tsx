b5bdc45a9828f37c56374bb53b25164bdeffe322
Eduardo Kegler <eduardo.kegler@coinbase.com>
2024-08-07 23:28:05 -0300

    feat: added clean button

--- src/components/AccountAvatar.tsx
+++ src/components/AccountAvatar.tsx
@@ -1,14 +1,17 @@
 "use client";
-import { memo, useCallback } from "react";
+import { memo, useCallback, useState } from "react";
 import { Avatar, Button } from "flowbite-react";
 import { useOAuth, useOAuthDispatch } from "@/providers/OAuthProvider";
 import { IoMdLogOut } from "react-icons/io";
 import { logout } from "@/actions/logout";
+import { LogoutModal } from "./LogoutModal";
 export const AccountAvatar = memo(function AccountAvatar() {
+  const [visible, setVisible] = useState(false);
   const { nickName, level, profileIconId } = useOAuth();
   const { setConnected } = useOAuthDispatch();
 
   const handleLogout = useCallback(async () => {
+    setVisible(false);
     setConnected(false);
     logout();
   }, [setConnected]);
@@ -28,6 +31,11 @@ export const AccountAvatar = memo(function AccountAvatar() {
       <Button onClick={handleLogout} size={"sm"} color={"gray"}>
         <IoMdLogOut />
       </Button>
+      <LogoutModal
+        visible={visible}
+        onClose={() => setVisible(false)}
+        onConfirm={handleLogout}
+      />
     </div>
   );
 });
--- src/components/filters/Search.tsx
+++ src/components/filters/Search.tsx
@@ -7,12 +7,18 @@ import {
 import { CustomFlowbiteTheme, TextInput } from "flowbite-react";
 import { useTranslations } from "next-intl";
 import React, { ChangeEvent, memo, useCallback } from "react";
+import { PrimaryButton } from "../PrimaryButton";
+import { MdClear } from "react-icons/md";
 
 const customTheme: CustomFlowbiteTheme["textInput"] = {
   field: {
     input: {
+      base: "block w-full border disabled:cursor-not-allowed disabled:opacity-50 h-full rounded-r-none",
       colors: {
         gray: "border-gray-300 text-gray-900 focus:border-cyan-500 focus:ring-cyan-500 dark:border-gray-600 dark:text-white dark:placeholder-gray-400 dark:focus:border-cyan-500 dark:focus:ring-cyan-500 bg-transparent",
+      },
+      withAddon: {
+        off: "rounded-lg rounded-r-none",
       },
     },
   },
@@ -32,16 +38,24 @@ export const Search = memo(function Search() {
   );
 
   return (
-    <TextInput
-      theme={customTheme}
-      width={"100%"}
-      id="small"
-      type="text"
-      sizing="sm"
-      placeholder={translate("placeholder")}
-      value={search}
-      onChange={handleSearch}
-      className="w-[280px]"
-    />
+    <div className="flex w-[280px]">
+      <TextInput
+        theme={customTheme}
+        id="small"
+        type="text"
+        sizing="sm"
+        placeholder={translate("placeholder")}
+        value={search}
+        onChange={handleSearch}
+        className="w-full"
+      />
+      <PrimaryButton
+        onClick={() => setSearch("")}
+        size={"xs"}
+        className="rounded-l-none flex justify-center items-center"
+      >
+        <MdClear className="h-4 w-4" />
+      </PrimaryButton>
+    </div>
   );
 });
