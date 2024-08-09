ddd0d0c109b14d918b337a44b15f2e7ba1e02091
Eduardo Kegler <eduardo.kegler@coinbase.com>
2024-08-08 22:05:48 -0300

    feat: add missing translations

--- src/actions/redirect.ts
+++ src/actions/redirect.ts
@@ -1,6 +1,10 @@
 import { ROUTE } from "@/contants";
 import { redirect } from "next/navigation";
 
+export async function homepage() {
+  redirect(ROUTE.HOME);
+}
+
 export async function collection() {
   redirect(ROUTE.COLLECTION);
 }
